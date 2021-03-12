39d14500f320bf20a5dad579b5cbd45ca585d2d7
Dmitry <novogrudskiy.dv@gmail.com>
2021-03-12 17:25:27 +0300

    react webpack app

---
+++ react-webpack-template/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import './style.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+                <h1>With my own webpack!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
